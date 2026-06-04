cc89537beef64fbe6548b59596ec9d3f89b14a7b
Amarendra Baitha <kumaramarendra555@gmail.com>
2026-06-04 09:04:52 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
-function toggleAbout(){
- const c=document.getElementById('aboutMore');
- const b=document.querySelector('.see-more-btn');
- c.classList.toggle('active');
- b.textContent=c.classList.contains('active')?'Show Less':'See More';
+function toggleAbout() {
+          const content = document.getElementById("aboutMore");
+          const text = document.getElementById("aboutBtnText");
+
+          if (window.getComputedStyle(content).display === "none") {
+                    content.style.display = "block";
+                    text.innerText = "Read Less";
+          } else {
+                    content.style.display = "none";
+                    text.innerText = "Read More";
+          }
+}
+function toggleProject(id) {
+          const content = document.getElementById(id);
+
+          if (content.style.display === "block") {
+                    content.style.display = "none";
+          } else {
+                    content.style.display = "block";
+          }
+}
+function toggleTable(id) {
+          const content = document.getElementById(id);
+
+          if (content.style.display === "block") {
+                    content.style.display = "none";
+          } else {
+                    content.style.display = "block";
+          }
 }
